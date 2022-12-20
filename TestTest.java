f62ccfd41f7a1b7a8aae578b9a0da63153bafcf7
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 <deltong918@Gmail.com>
2022-12-20 11:57:30 +0800

    no message

---
+++ TestTest.java
@@ -0,0 +1,22 @@
+public class TestTest{
+	public static void main(String[] args) {
+		int A;
+		A = 1;
+		int B = 2;
+		System.out.println(B);
+		B = 3;
+		System.out.println(A);
+		System.out.println(B);
+		
+		byte age = 20;
+		double score = 99.99;
+		char gender = '男';
+		String name = "Gali";
+		System.out.println("信息如下\nname:" + name );
+		System.out.println("score:" + score);
+		System.out.println("gender:" + gender);
+		System.out.println("age:" + age);
+		
+
+	}
+}
